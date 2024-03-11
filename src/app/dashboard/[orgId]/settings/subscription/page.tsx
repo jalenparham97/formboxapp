2759e0ad0a9bb8089b1aa64e7aa8bbaa66b6a655
Jalen Parham <jalenparham97@gmail.com>
2024-03-10 22:04:02 -0400

    Updating subscription page design

--- src/app/dashboard/[orgId]/settings/subscription/page.tsx
+++ src/app/dashboard/[orgId]/settings/subscription/page.tsx
@@ -1,6 +1,8 @@
 import ManageSubscriptionButton from "@/components/subscription/manage-subscription-button";
 import PricingSection from "@/components/subscription/pricing-section";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Divider } from "@/components/ui/divider";
 import { api } from "@/trpc/server";
 
 interface Props {
@@ -16,38 +18,62 @@ export default async function SettingsSubscriptionPage({
   const currentPlan = org?.stripePlanNickname?.split("-")[0] ?? "Free";
 
   return (
-    <div className="space-y-10">
-      <div>
-        <h2 className="text-lg font-semibold leading-7 text-gray-900">
-          Subscription
-        </h2>
-        <p className="mt-1 leading-6 text-gray-600">
-          View and edit your billing details, as well as cancel your
-          subscription.
-        </p>
-        <p className="mt-3">
-          Your current plan:{" "}
-          <span className="font-semibold capitalize">{currentPlan}</span>
-        </p>
-        <div className="mt-5">
-          <ManageSubscriptionButton orgId={orgId} />
+    <div>
+      <div className="">
+        <div>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Subscription
+          </h2>
+          <p className="mt-1 leading-6 text-gray-600">
+            View and edit your billing details, as well as cancel your
+            subscription.
+          </p>
         </div>
-      </div>
 
-      <div className="lg:max-w-7xl">
-        <PricingSection products={products} org={org} />
-      </div>
+        <div className="mt-6">
+          <Card>
+            <div>
+              <div className="p-6">
+                <div>
+                  <h2 className="text-lg font-semibold">Current plan</h2>
+                  <p className="mt-2 text-gray-600">
+                    Your current plan:{" "}
+                    <span className="font-semibold capitalize text-gray-900">
+                      {currentPlan}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <Divider />
+              <div className="p-6">
+                <ManageSubscriptionButton orgId={orgId} />
+              </div>
+            </div>
+          </Card>
+        </div>
 
-      <div>
-        <h2 className="text-lg font-semibold leading-7 text-gray-900">
-          Need anything else?
-        </h2>
-        <p className="mt-1 leading-6 text-gray-600">
-          If you need any further help with billing, our support team are here
-          to help.
-        </p>
-        <div className="mt-3">
-          <Button variant="outline">Contact support</Button>
+        <div className="mt-10 lg:max-w-7xl">
+          <PricingSection products={products} org={org} />
+        </div>
+
+        <div className="mt-10">
+          <Card>
+            <div>
+              <div className="p-6">
+                <div>
+                  <h2 className="text-lg font-semibold">Need anything else?</h2>
+                  <p className="mt-2 text-gray-600">
+                    If you need any further help with billing, our support team
+                    are here to help.
+                  </p>
+                </div>
+              </div>
+              <Divider />
+              <div className="p-6">
+                <Button variant="outline">Contact support</Button>
+              </div>
+            </div>
+          </Card>
         </div>
       </div>
     </div>
--- src/components/subscription/pricing-section.tsx
+++ src/components/subscription/pricing-section.tsx
@@ -12,6 +12,8 @@ import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/utils/format-currency";
 import { api } from "@/trpc/react";
+import { Divider } from "../ui/divider";
+import Link from "next/link";
 
 type PriceFrequency = "month" | "year";
 
@@ -46,85 +48,96 @@ export default function PricingSection({ products, org }: Props) {
 
   return (
     <div>
-      <div className="">
-        <div>
-          <h2 className="text-lg font-semibold leading-7 text-gray-900">
-            Plans
-          </h2>
-          <p className="mt-1 leading-6 text-gray-600">
-            Choose the plan thats right for you.
-          </p>
-        </div>
-        <div className="mt-3">
-          <SegmentedControls
-            defaultValue={frequency}
-            onValueChange={(frequency) =>
-              setFrequency(frequency as PriceFrequency)
-            }
-          >
-            <SegmentedControlsList>
-              <SegmentedControlsTrigger value="month">
-                Monthly
-              </SegmentedControlsTrigger>
-              <SegmentedControlsTrigger value="year">
-                Yearly
-              </SegmentedControlsTrigger>
-            </SegmentedControlsList>
-          </SegmentedControls>
+      <Card>
+        <div className="p-6">
+          <div>
+            <h2 className="text-lg font-semibold leading-7 text-gray-900">
+              Plans
+            </h2>
+            <p className="mt-1 leading-6 text-gray-600">
+              Choose the plan thats right for you.
+            </p>
+          </div>
+          <div className="mt-3">
+            <SegmentedControls
+              defaultValue={frequency}
+              onValueChange={(frequency) =>
+                setFrequency(frequency as PriceFrequency)
+              }
+            >
+              <SegmentedControlsList>
+                <SegmentedControlsTrigger value="month">
+                  Monthly
+                </SegmentedControlsTrigger>
+                <SegmentedControlsTrigger value="year">
+                  Yearly
+                </SegmentedControlsTrigger>
+              </SegmentedControlsList>
+            </SegmentedControls>
 
-          <div className="mt-6 w-full space-y-6 lg:flex lg:justify-between lg:space-x-3 lg:space-y-0">
-            {products
-              ?.sort(
-                (pA, pB) =>
-                  Number(pA?.prices?.month?.price) -
-                  Number(pB?.prices?.month?.price),
-              )
-              .map((product) => (
-                <Card
-                  className="w-full border border-gray-300 p-5"
-                  key={product.id}
-                >
-                  <div>
-                    <h3 className="text-xl font-semibold">{product.name}</h3>
-                    <p className="text-dark-300 mt-2 text-sm leading-normal">
-                      {product.description}
-                    </p>
+            <div className="mt-6 w-full space-y-6 lg:flex lg:space-x-10 lg:space-y-0">
+              {products
+                ?.sort(
+                  (pA, pB) =>
+                    Number(pA?.prices?.month?.price) -
+                    Number(pB?.prices?.month?.price),
+                )
+                .map((product) => (
+                  <div className="" key={product.id}>
+                    <div>
+                      <h3 className="text-xl font-semibold">{product.name}</h3>
+                      <p className="mt-2 leading-normal text-gray-600">
+                        {product.description}
+                      </p>
+                    </div>
+
+                    <div>
+                      <p className="mt-6 flex items-baseline gap-x-1">
+                        <span className="text-3xl font-bold tracking-tight text-gray-900">
+                          {formatCurrency(product.prices[frequency].price) ||
+                            "$0"}
+                        </span>
+                        <span className="text-sm font-semibold leading-loose text-gray-600">
+                          / {frequency}
+                        </span>
+                      </p>
+
+                      {!org?.stripeSubscriptionId && (
+                        <div className="mt-6">
+                          <Button
+                            onClick={() =>
+                              handleUpgrade(
+                                product.prices[frequency].id as string,
+                              )
+                            }
+                            disabled={
+                              !org?.stripeSubscriptionId &&
+                              product.name.toLowerCase() === "free"
+                            }
+                          >
+                            Get plan
+                          </Button>
+                        </div>
+                      )}
+                    </div>
                   </div>
-
-                  <div>
-                    <p className="mt-6 flex items-baseline gap-x-1">
-                      <span className="text-3xl font-bold tracking-tight text-gray-900">
-                        {formatCurrency(product.prices[frequency].price) ||
-                          "$0"}
-                      </span>
-                      <span className="text-sm font-semibold leading-loose text-gray-600">
-                        / {frequency}
-                      </span>
-                    </p>
-
-                    {!org?.stripeSubscriptionId && (
-                      <div className="mt-6">
-                        <Button
-                          onClick={() =>
-                            handleUpgrade(
-                              product.prices[frequency].id as string,
-                            )
-                          }
-                          disabled={
-                            !org?.stripeSubscriptionId &&
-                            product.name.toLowerCase() === "free"
-                          }
-                        >
-                          Get plan
-                        </Button>
-                      </div>
-                    )}
-                  </div>
-                </Card>
-              ))}
+                ))}
+            </div>
           </div>
         </div>
-      </div>
+
+        <Divider />
+
+        <div className="p-6">
+          <Link
+            href="https://formbox.app/pricing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button variant="outline">View full pricing table</Button>
+          </Link>
+        </div>
+      </Card>
     </div>
   );
 }
