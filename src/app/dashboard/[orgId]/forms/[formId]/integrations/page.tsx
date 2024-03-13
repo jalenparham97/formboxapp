5a74c899487c8f8c6d5de278fa3a93cf2d88d1ff
Jalen Parham <jalenparham97@gmail.com>
2024-03-13 00:18:57 -0400

    Adding dynamic metadate titles

--- src/app/dashboard/[orgId]/forms/[formId]/integrations/page.tsx
+++ src/app/dashboard/[orgId]/forms/[formId]/integrations/page.tsx
@@ -1,5 +1,15 @@
+import { api } from "@/trpc/server";
+import { type Metadata } from "next";
+
 interface Props {
   params: { formId: string; orgId: string };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const form = await api.form.getById.query({ id: params.formId });
+  return {
+    title: `Integrations - ${form?.name}`,
+  };
 }
 
 export default function IntegrationsPage({ params: { orgId, formId } }: Props) {
