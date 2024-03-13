dc35644ed6c969fdeeb82a24058b92ace8af7481
Jalen Parham <jalenparham97@gmail.com>
2024-03-13 17:02:52 -0400

    Adding prisma accelerate extention

--- src/env.js
+++ src/env.js
@@ -11,8 +11,15 @@ export const env = createEnv({
       .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        (str) => !str.includes("YOUR_DB_URL_HERE"),
         "You forgot to change the default URL",
+      ),
+    DIRECT_DATABASE_URL: z
+      .string()
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_DIRECT_DB_URL_HERE"),
+        "You forgot to change the default direct URL",
       ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -67,6 +74,7 @@ export const env = createEnv({
   runtimeEnv: {
     // Server ENV
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_DATABASE_URL: process.env.DIRECT_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { env } from "@/env";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+  }).$extends(withAccelerate());
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
